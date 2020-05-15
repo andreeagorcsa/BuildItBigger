db225c0a327fc224d4ececb02ec970f4f8cffbf8
andreeagorcsa <andreea.gorcsa@t-systems.com>
2020-05-15 18:53:03 +0200

    Initial commit

---
+++ jokeandroidlibrary/src/main/java/com/example/jokeandroidlibrary/JokeUtil.java
@@ -0,0 +1,12 @@
+package com.example.jokeandroidlibrary;
+
+import android.util.Log;
+
+public class JokeUtil {
+
+    public static final String LOG_TAG = "Joke Android Library";
+
+    public static void showMessage(String message) {
+        Log.d(LOG_TAG, message);
+    }
+}
